c97e39d47a7fe4f5cf5fce45b4969ba59e36d480
lee <leeyoo@01w.kr>
2023-11-17 17:42:14 +0900

    fix: category schema

--- app/(dashboard)/[storeId]/(routes)/categories/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -5,12 +5,12 @@ import prismadb from "@/lib/prismadb";
 import { BillboardClient } from "./components/client";
 import { BillboardColumn } from "./components/columns";
 
-const BillboardsPage = async ({
+const CategoriesPage = async ({
   params
 }: {
   params: { storeId: string }
 }) => {
-  const billboards = await prismadb.billboard.findMany({
+  const categories = await prismadb.category.findMany({
     where: {
       storeId: params.storeId
     },
@@ -19,19 +19,19 @@ const BillboardsPage = async ({
     }
   });
 
-  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+  const formattedCategories: BillboardColumn[] = categories.map((item) => ({
     id: item.id,
-    label: item.label,
+    label: item.name,
     createdAt: format(item.createdAt, "MMMM do, yyyy")
   }));
 
   return ( 
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={formattedBillboards} />
+        <BillboardClient data={formattedCategories} />
       </div>
     </div>
    );
 }
  
-export default BillboardsPage;
+export default CategoriesPage;
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -24,6 +24,11 @@ export function MainNav({
       active: pathname === `/${params.storeId}/billboards`,
     },
     {
+      href: `/${params.storeId}/categories`,
+      lable: 'Categories',
+      active: pathname === `/${params.storeId}/categories`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       lable: 'Settings',
       active: pathname === `/${params.storeId}/settings`,
